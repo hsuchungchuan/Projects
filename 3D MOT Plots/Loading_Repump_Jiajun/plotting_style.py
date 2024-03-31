3a794acbf8ac63c4dcaf8baead25db86ac45b10c
hsuchungchuan <cch56@cam.ac.uk>
2024-03-31 23:47:20 +0100

    change plotting style

--- 3D MOT Plots/Loading_Repump_Jiajun/plotting_style.py
+++ 3D MOT Plots/Loading_Repump_Jiajun/plotting_style.py
@@ -10,11 +10,9 @@ print('Initialising plotting style...')
 
 def set_plotting_style():
     plt.rc("font", family = 'Arial', size=10)
-    plt.rc('axes', labelsize=20, labelpad=5)
+    plt.rc('axes', labelsize=20, titlesize = 20, labelpad=5)
     plt.rc('xtick', labelsize=18)
     plt.rc('ytick', labelsize=18)
-    plt.rc('title', labelsize=20)
     plt.rc('legend', fontsize=15)
-
 
 print('Success! Plotting style initialised.')
