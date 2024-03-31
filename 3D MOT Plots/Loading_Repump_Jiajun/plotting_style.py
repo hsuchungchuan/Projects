f3681726b2f22f2b0a4a260f3ed2bb071b38de99
hsuchungchuan <cch56@cam.ac.uk>
2024-03-31 23:37:27 +0100

    Started 3D MOT plots

---
+++ 3D MOT Plots/Loading_Repump_Jiajun/plotting_style.py
@@ -0,0 +1,17 @@
+## On your .ipynb, copy the plotting_style.py file to the same directory and add these to the .ipynb:
+
+#import matplotlib.pyplot as plt
+#from plotting_style import set_plotting_style
+#set_plotting_style()
+
+import matplotlib.pyplot as plt
+
+print('Initialising plotting style...')
+
+def set_plotting_style():
+    plt.rc("font", family = 'Arial', size=10)
+    plt.rc('axes', labelsize=20, labelpad=5)
+    plt.rc('xtick', labelsize=18)
+    plt.rc('ytick', labelsize=18)
+
+print('Success! Plotting style initialised.')
