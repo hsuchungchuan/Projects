2d9bd2410c04784cb0aa503dc47db8d9e534e7fc
hsuchungchuan <cch56@cam.ac.uk>
2024-03-31 23:55:22 +0100

    made a global plotting_style.py script that is called for all projects inside for consistent font. it will be called via directoyr manipulation in the script

--- plotting_style.py
+++ plotting_style.py
@@ -1,8 +1,10 @@
-## On your .ipynb, copy the plotting_style.py file to the same directory and add these to the .ipynb:
+## On your .ipynb, import this plotting_style by adding in and uncommenting the following lines:
 
-#import matplotlib.pyplot as plt
-#from plotting_style import set_plotting_style
-#set_plotting_style()
+# import matplotlib.pyplot as plt
+# import sys
+# sys.path.append('C:/Users/Michael/Dropbox/AION/Projects')  # Replace with the relevant path that contains this script
+# from plotting_style import set_plotting_style
+# set_plotting_style()
 
 import matplotlib.pyplot as plt
 
@@ -10,8 +12,9 @@ print('Initialising plotting style...')
 
 def set_plotting_style():
     plt.rc("font", family = 'Arial', size=10)
-    plt.rc('axes', labelsize=20, labelpad=5)
+    plt.rc('axes', labelsize=20, titlesize = 20, labelpad=5)
     plt.rc('xtick', labelsize=18)
     plt.rc('ytick', labelsize=18)
+    plt.rc('legend', fontsize=15)
 
 print('Success! Plotting style initialised.')
