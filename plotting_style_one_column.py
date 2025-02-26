32e302b865bce76c066e8d0ad1901e411866b2f0
hsuchungchuan <cch56@cam.ac.uk>
2025-02-26 02:24:29 +0000

    made a one column single figure plotting style :D

---
+++ plotting_style_one_column.py
@@ -0,0 +1,48 @@
+# copy paste this code to the beginning of your script to set the plotting style for a three column figure
+
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"]
+})
+
+plt.rcParams["savefig.bbox"] = None
+plt.rcParams["figure.figsize"] = (5.9,4.2)
+plt.rcParams["figure.titlesize"] = 11 # this is only for plt.suptitle, i set it to be the same as the rest
+
+plt.rcParams["axes.titlesize"] = 11 # control title size
+plt.rcParams["axes.labelsize"] = 11 # control x and y axis labels
+plt.rcParams["axes.labelpad"] = 4.0 # distance between axis label and axis numbers
+plt.rcParams["xtick.labelsize"] = 11 # independent control of xlabel fontsize
+plt.rcParams["ytick.labelsize"] = 11 # independent control of ylabel fontsize
+
+plt.rcParams["legend.fontsize"] = 7.5 # control legend size, I set it to be slightly smaller than title/axes label
+# plt.rcParams["legend.facecolor"] = (0.9, 0.9, 0.9) # legend background color
+# plt.rcParams["legend.edgecolor"] = (0, 0, 0) # legend border line color
+# plt.rcParams["legend.frameon"] = True # whether or not to draw a border around legend
+# plt.rcParams["legend.framealpha"] = 0.1 # legend background transparency
+plt.rcParams["legend.loc"] = "upper left" # default legend location
+plt.rcParams["legend.handletextpad"] = 0.8  # Horizontal space between legend point and text (default 0.8)
+plt.rcParams["legend.labelspacing"] = 0.6  # Vertical space between legend entries (default 0.5)
+# plt.rcParams["legend.columnspacing"] = 2.0  # Space between columns in multi-column legends
+# plt.rcParams["legend.borderpad"] = 1.0  # Space between legend content and the box
+# plt.rcParams["legend.handlelength"] = 2.0  # Length of legend marker lines
+
+plt.rcParams["lines.linewidth"] = 1
+# plt.rcParams["lines.markeredgewidth"] = 0.7
+plt.rcParams["lines.markersize"] = 4 #data point size
+
+plt.rcParams["grid.linestyle"] = "--"
+plt.rcParams['axes.grid'] = True
+# plt.rcParams["grid.linewidth"] = 0.7
+
+# plt.rcParams["xtick.major.pad"] = 4.0 # distance between xticks dashes and xaxis numbers
+# plt.rcParams["xtick.major.size"] = 3.0
+# plt.rcParams["xtick.major.width"] = 0.7
+# plt.rcParams["ytick.major.pad"] = 4.0 # distance between yticks dashes and yaxis numbers
+# plt.rcParams["ytick.major.size"] = 3.0
+# plt.rcParams["ytick.major.width"] = 0.7
+
+# plt.subplots_adjust(left=0.2, right=0.8, top=0.8, bottom=0.2) # remove unnecessary white space how i like it
