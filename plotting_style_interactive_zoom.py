36a64341bfc25d6668b85b6e9aea8edf475fd6fa
hsuchungchuan <cch56@cam.ac.uk>
2025-03-23 22:21:37 +0000

    plotted out the wavelength drift long term time trace of 241201 pdh locked laser (WM data)

---
+++ plotting_style_interactive_zoom.py
@@ -0,0 +1,5 @@
+# for interactive plot where you can zoom in and stuff, use this code instead
+
+import matplotlib
+matplotlib.use('TkAgg')  # Set the interactive backend
+import matplotlib.pyplot as plt
